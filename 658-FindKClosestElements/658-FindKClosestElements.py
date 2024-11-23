fcd9289f49f05d6c63e39f8c34a2e849eee5dd0d
Arafat04H09 <91175479+Arafat04H09@users.noreply.github.com>
2024-11-23 15:50:59 -0500

    Update file 11/23/2024, 3:49:37 PM

--- 658-FindKClosestElements/658-FindKClosestElements.py
+++ 658-FindKClosestElements/658-FindKClosestElements.py
@@ -1,21 +1,9 @@
-class Solution(object):
-    def findClosestElements(self, arr, k, x):
-        """
-        :type arr: List[int]
-        :type k: int
-        :type x: int
-        :rtype: List[int]
-        """
-        heap = []
-        
-        for i in range(k):
-            heapq.heappush(heap, (-abs(arr[i] - x), -arr[i]))
-        
-        for i in range(k, len(arr)):
-            diff = abs(arr[i] - x)
-            if diff < -heap[0][0] or (diff == -heap[0][0] and arr[i] < -heap[0][1]):
-                heapq.heappushpop(heap, (-diff, -arr[i]))
-        
-        result = sorted(-num for _, num in heap)
-        return result
-
+    def findClosestElements(self, A, k, x):
+        left, right = 0, len(A) - k
+        while left < right:
+            mid = (left + right) / 2
+            if x - A[mid] > A[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        return A[left:left + k]
