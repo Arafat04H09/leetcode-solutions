29887763c5d66f562d13c4dd2c66581cd29e9e6e
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2024-12-03 23:46:05 -0500

    Create file

---
+++ 2825-MakeStringaSubsequenceUsingCyclicIncrements/2825-MakeStringaSubsequenceUsingCyclicIncrements.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def canMakeSubsequence(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: bool
+        """
+        if len(str2) > len(str1):
+            return False
+
+        i2 = 0
+
+        def nextChar(ch):
+            if ch == 'z':
+                return 'a'
+            else:
+                return chr(ord(ch) + 1)
+
+        for _, ch in enumerate(str1):
+            if ch == str2[i2] or nextChar(ch) == str2[i2]:
+                i2 += 1
+            if i2 == len(str2):
+                return True
+        
+        return False
+
+
