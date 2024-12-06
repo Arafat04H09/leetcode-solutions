5f4f9edfdee3e816f47ad31e6ed31dc41208cd71
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2024-12-06 15:16:21 -0500

    Update file 12/6/2024, 3:14:58 PM

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
@@ -1,17 +1,8 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if len(nums) < 3:
-            return len(nums)
-        
-        slow = 2
-
-        for fast in range(2, len(nums)):
-            if nums[fast] != nums[slow - 2]:
-                nums[slow] = nums[fast]
-                slow += 1 
-
-        return slow 
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        for i in range(1, len(nums)):
+            if j == 1 or nums[i] != nums[j - 2]:
+                nums[j] = nums[i]
+                j += 1
+        return j
