c10445e2145b0b781f05f6e7f5cb45344a8ece1b
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2024-12-05 19:29:27 -0500

    Update file 12/5/2024, 7:28:03 PM

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.py
@@ -1,14 +1,13 @@
-class Solution(object):
-    def findDisappearedNumbers(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        n = set(nums)
-        ans = []
-
-        for i in range(1, len(nums) + 1):
-            if i not in n:
-                ans.append(i)
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            temp = abs(nums[i]) - 1
+            if nums[temp] > 0:
+                nums[temp] *= -1
         
-        return ans
+        res = []
+        for i,n in enumerate(nums):
+            if n > 0:
+                res.append(i+1)
+        
+        return res
