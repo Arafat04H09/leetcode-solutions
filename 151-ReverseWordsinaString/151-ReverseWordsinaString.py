07e4c0d6c72105a2235e10abea0063c524cf981f
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2024-12-08 17:00:39 -0500

    Update file 12/8/2024, 4:59:15 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.py
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.py
@@ -4,22 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        ans = ''
-        curIndex = 0
-        length = len(s) 
+        s = s.split()
 
-        while curIndex < length:
-            #skip over until you reach a letter
-            while curIndex < length and not s[curIndex].isalnum():
-                curIndex += 1
-            
-            #you have reached a letter or index > length
-            word = ''
-            while curIndex < length and s[curIndex].isalnum():
-                word += s[curIndex]
-                curIndex += 1
-            
-            #insert word to beginning of sentence
-            ans = ' '+ word + ans
+        ans = ''
+
+        for word in s[::-1]:
+            ans += word + ' '
         
         return ans.strip()
