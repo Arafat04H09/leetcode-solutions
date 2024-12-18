a684e7e3735f5ae1f25f690ca2b2bcf1201095ea
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2024-12-18 15:06:54 -0500

    Create file

---
+++ 1475-FinalPricesWithaSpecialDiscountinaShop/1475-FinalPricesWithaSpecialDiscountinaShop.py
@@ -0,0 +1,31 @@
+#THE INTUITION OF USING 2 POINTERS COME FROM THE FACT THAT FOR ANY ITH VALUE IN ANSWER WE NEED TO ACCESS TWO VALUES IN INPUT ARRAY
+    i = 0 
+    j = i +1
+    
+    while i <j and j < len(prices) :
+        #STEP 1: INCREASE I TO  NEXT INDEX AND J NEXT TO I IF ITH ELEMENT GREATER THAN JTH AFTER PERFORMING THE OPERATION
+        if prices[i] >= prices[j]:
+            
+            prices[i] = prices[i] - prices[j]
+            i = i +1
+            j = i+1
+        
+        else:
+            
+        #STEP2:  IF THERE IS NO JTH ELEMENT WHICHH IS SMALLER THAN ITH AND YOU REACH END OF ARRAY THEN INCREMENT i and shift back j to i+1
+        """EXAMPLE: Input    [4,7,1,9,4,8,8,9,4]
+                    Output   [3,6,1,9,4,8,8,9,4]
+                    Expected [3,6,1,5,0,0,4,5,4]"""
+        
+            if (j == len(prices)-1):
+                i = i +1
+                j = i +1
+                
+        # STEP3 :ELSE INCREASE j by 1 and re enter in to step 1 if condition satisfies
+            else:
+                j = j +1
+    return prices
+        
+        
+        
+    
