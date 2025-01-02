93c617f143ae49e2f313ddec86e1f2f449aab52f
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-02 17:03:26 -0500

    Update file 1/2/2025, 5:01:41 PM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -2,16 +2,19 @@ class Solution(object):
     def climbStairs(self, n):
         if n == 1:
             return 1
+        
         if n == 2:
             return 2
-        dp = [0] * (n)
-        dp[0] = 1
-        dp[1] = 2
 
-        for i in range(2, n):
-            dp[i] = dp[i - 1] + dp[i - 2]
+        prev = 2
+        prev2 = 1
+
+        for i in range(3, n+1):
+            cur = prev + prev2
+            prev2 = prev
+            prev = cur
         
-        return dp[-1]
+        return cur
         
 
 
