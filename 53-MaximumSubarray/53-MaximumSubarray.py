cee47832742f5c47239070d556b30cdb136a5ade
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-05 01:56:44 -0500

    Update file 1/5/2025, 1:55:01 AM

---
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -0,0 +1,10 @@
+public int maxSubArray(int[] nums) {
+	if (nums == null || nums.length == 0) { return 0; }
+	int max = nums[0], sum = nums[0];
+	for (int i = 1; i < nums.length; i++) {
+		if (sum < 0) { sum = nums[i]; }
+		else {sum += nums[i]; }
+		max = Math.max(max, sum);
+	}
+	return max;
+}
