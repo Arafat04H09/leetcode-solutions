b8f0569b3fab17ca2d335fcd5e394579cc0ff1c5
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-05 01:40:12 -0500

    Update file 1/5/2025, 1:38:29 AM

--- 152-MaximumProductSubarray/152-MaximumProductSubarray.py
+++ 152-MaximumProductSubarray/152-MaximumProductSubarray.py
@@ -1,17 +1,6 @@
-class Solution(object):
-    def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        dpMax = [0] * len(nums)
-        dpMin = [0] * len(nums)
-        dpMax[0] = nums[0]
-        dpMin[0] = nums[0]
-
-        for i in range(1, len(nums)):
-            dpMax[i] = max(dpMax[i - 1] * nums[i], dpMin[i -1] * nums[i], nums[i])
-            dpMin[i] = min(dpMax[i - 1] * nums[i], dpMin[i -1] * nums[i], nums[i])
-        
-        
-        return max(max(dpMax), max(dpMin))
+    def maxProduct(self, A):
+        B = A[::-1]
+        for i in range(1, len(A)):
+            A[i] *= A[i - 1] or 1
+            B[i] *= B[i - 1] or 1
+        return max(A + B)
