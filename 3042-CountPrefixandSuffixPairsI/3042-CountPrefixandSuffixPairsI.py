75a452c9752ac75bc7021e51076f66e3bef6a3a6
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-07 21:29:58 -0500

    Create file

---
+++ 3042-CountPrefixandSuffixPairsI/3042-CountPrefixandSuffixPairsI.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countPrefixSuffixPairs(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                prefix = words[i]
+                suffix = words[i]
+                word = words[j]
+                
+                if word.startswith(prefix) and word.endswith(suffix):
+                    count += 1
+
+        return count
