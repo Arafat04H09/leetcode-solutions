a5af07323ea9d995d5f07bdfe624147773e2fed1
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-08 20:55:37 -0500

    Update file 1/8/2025, 8:53:51 PM

--- 2545-SorttheStudentsbyTheirKthScore/2545-SorttheStudentsbyTheirKthScore.py
+++ 2545-SorttheStudentsbyTheirKthScore/2545-SorttheStudentsbyTheirKthScore.py
@@ -1,15 +1,1 @@
-class Solution(object):
-    def sortTheStudents(self, score, k):
-        """
-        :type score: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
-        arr = []
-
-        for i, scores in enumerate(score):
-            arr.append((scores[k], i))
-        
-        arr.sort(reverse = True)
-
-        return [score[index] for _,index in arr]
+    return sorted(score, key=itemgetter(k), reverse=True)
