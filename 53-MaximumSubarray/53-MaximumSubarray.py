516357230b00c4fdf335e14e29e7dcc12c6748ca
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-08 22:37:35 -0500

    Update file 1/8/2025, 10:35:49 PM

--- 53-MaximumSubarray/53-MaximumSubarray.py
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -1,10 +1,14 @@
-public int maxSubArray(int[] nums) {
-	if (nums == null || nums.length == 0) { return 0; }
-	int max = nums[0], sum = nums[0];
-	for (int i = 1; i < nums.length; i++) {
-		if (sum < 0) { sum = nums[i]; }
-		else {sum += nums[i]; }
-		max = Math.max(max, sum);
-	}
-	return max;
-}
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        m = -float("inf")
+        c = 0
+
+        for num in nums:
+            c = max(c + num, num)
+            m = max(m, c)
+    
+        return m
