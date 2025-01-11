f4e07b3dacc3c076d2464b5f86f2cb6d96d439d3
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-10 21:14:12 -0500

    Create file

---
+++ 721-AccountsMerge/721-AccountsMerge.py
@@ -0,0 +1,6 @@
+[
+  ["John", "johnsmith@mail.com", "john00@mail.com"], # Account 0
+  ["John", "johnnybravo@mail.com"], # Account 1
+  ["John", "johnsmith@mail.com", "john_newyork@mail.com"],  # Account 2
+  ["Mary", "mary@mail.com"] # Account 3
+]
