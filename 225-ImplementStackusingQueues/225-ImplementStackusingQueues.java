9c864d24ee27c018d58228a6bdb30acce19e09d1
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-16 19:01:55 -0500

    Create file

---
+++ 225-ImplementStackusingQueues/225-ImplementStackusingQueues.java
@@ -0,0 +1,43 @@
+class MyStack {
+    private Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        queue.add(x);
+
+        for (int i = 1; i < queue.size(); i++) {
+            queue.add(queue.poll());
+        }
+    }
+    
+    public int pop() {
+        if (queue.isEmpty()) {
+            return -1;
+        }
+
+        return queue.poll();
+    }
+    
+    public int top() {
+        if (queue.isEmpty()) {
+            return -1;
+        }
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
