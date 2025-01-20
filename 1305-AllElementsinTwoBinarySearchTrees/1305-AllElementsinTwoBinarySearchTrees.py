bcc784b35aab69960348987a25dc7f44013c943b
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-20 12:54:32 -0500

    Update file 1/20/2025, 12:52:56 PM

--- 1305-AllElementsinTwoBinarySearchTrees/1305-AllElementsinTwoBinarySearchTrees.py
+++ 1305-AllElementsinTwoBinarySearchTrees/1305-AllElementsinTwoBinarySearchTrees.py
@@ -11,17 +11,25 @@ class Solution(object):
         :type root2: Optional[TreeNode]
         :rtype: List[int]
         """
+        st1 = []
+        st2 = []
         ans = []
 
-        def helper(node):
-            if not node:
-                return
+        while (root1 or root2 or st1 or st2):
+            while root1:
+                st1.append(root1)
+                root1 = root1.left
+            while root2:
+                st2.append(root2)
+                root2 = root2.left
+            
+            if (not st2 or st1 and st1[-1].val <= st2[-1].val):
+                root1 = st1.pop()
+                ans.append(root1.val)
+                root1 = root1.right
+            else:
+                root2 = st2.pop()
+                ans.append(root2.val)
+                root2 = root2.right
 
-            helper(node.left)
-            heapq.heappush(ans, node.val)
-            helper(node.right)
-        
-        helper(root1)
-        helper(root2)
-
-        return sorted(ans)
+        return ans
