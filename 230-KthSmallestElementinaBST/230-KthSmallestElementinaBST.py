d24035ffce9b1642626908c9e823c51a7d4d8496
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-20 13:07:00 -0500

    Update file 1/20/2025, 1:05:25 PM

--- 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.py
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.py
@@ -1,25 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def kthSmallest(self, root, k):
         """
-        :type root: Optional[TreeNode]
+        :type root: TreeNode
         :type k: int
         :rtype: int
         """
-        ans = []
-
-        def helper(node):
-            if not node:
-                return
-            
-            helper(node.left)
-            ans.append(node.val)
-            helper(node.right)
+        count = []
+        self.helper(root, count)
+        return count[k-1]
         
-        helper(root)
-        return ans[k- 1]
+    def helper(self, node, count):
+        if not node:
+            return
+        
+        self.helper(node.left, count)
+        count.append(node.val)
+        self.helper(node.right, count)
