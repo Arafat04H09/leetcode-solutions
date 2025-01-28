2acf37cd7b261d26d1a82d92e82513fc6690fc19
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-28 17:52:35 -0500

    Update file 1/28/2025, 5:51:03 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,22 +1,22 @@
-def longestPalindrome(self, s):
-        longest_palindrom = ''
-        dp = [[0]*len(s) for _ in range(len(s))]
-        #filling out the diagonal by 1
-        for i in range(len(s)):
+class Solution:
+    def longestPalindrome(self, s):
+        if len(s) == 0:
+            return ''
+
+        longest = s[0]
+        l = len(s)
+        dp = [[False] * l for _ in range(l)]
+
+        for i in range(l):
             dp[i][i] = True
-            longest_palindrom = s[i]
-			
-        # filling the dp table
-        for i in range(len(s)-1,-1,-1):
-				# j starts from the i location : to only work on the upper side of the diagonal 
-            for j in range(i+1,len(s)):  
-                if s[i] == s[j]:  #if the chars mathces
-                    # if len slicied sub_string is just one letter if the characters are equal, we can say they are palindomr dp[i][j] =True 
-                    #if the slicied sub_string is longer than 1, then we should check if the inner string is also palindrom (check dp[i+1][j-1] is True)
-                    if j-i ==1 or dp[i+1][j-1] is True:
-                        dp[i][j] = True
-                        # we also need to keep track of the maximum palindrom sequence 
-                        if len(longest_palindrom) < len(s[i:j+1]):
-                            longest_palindrom = s[i:j+1]
-                
-        return longest_palindrom
+        
+        for start in range(l - 1, -1, -1):
+            for end in range(start + 1, l):
+                if s[start] == s[end]:
+                    if end - start == 1 or dp[start + 1][end - 1]:
+                        dp[start][end] = True
+                    
+                        if end - start + 1 > len(longest):
+                            longest = s[start: end + 1]
+        
+        return longest
