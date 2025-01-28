c437cfeb7b6047ef5defbf85741763510863119c
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-28 18:24:07 -0500

    Update file 1/28/2025, 6:22:35 PM

--- 12-IntegertoRoman/12-IntegertoRoman.py
+++ 12-IntegertoRoman/12-IntegertoRoman.py
@@ -1,9 +1,12 @@
-class Solution:
-    def intToRoman(self, num: int) -> str:
-        Roman = ""
-        storeIntRoman = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
-        for i in range(len(storeIntRoman)):
-            while num >= storeIntRoman[i][0]:
-                Roman += storeIntRoman[i][1]
-                num -= storeIntRoman[i][0]
-        return Roman
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        thousands = ["", "M", "MM", "MMM"]
+        hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+
+        return thousands[num/1000] + hundreds[(num % 1000)/100] + tens[(num % 100)/10] + ones[num%10]
