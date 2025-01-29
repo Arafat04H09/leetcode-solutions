27a7507db53569127a409c0281ca1f00bffc90b5
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-29 12:49:05 -0500

    Update file 1/29/2025, 12:47:33 PM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
@@ -1,20 +1,27 @@
 class Solution(object):
     def letterCombinations(self, digits):
+        if len(digits) == 0:
+            return []
+            
         ans = []
 
-        if not digits:
-            return ans
+        m = {"2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz",
+        }
 
-        m = { '2' : "abc", '3' : 'def', '4' : 'ghi', '5' : 'jkl', '6' : 'mno', '7' : 'pqrs', '8': 'tuv', '9': 'wxyz'}
-
-        def backtrack(s, index):
+        def backtrack(index, combination):
             if index == len(digits):
-                ans.append(s)
+                ans.append(combination)
                 return
-
-            for ch in m[digits[index]]:
-                backtrack(s + ch, index + 1)
+            
+            for nextLetter in m[digits[index]]:
+                backtrack(index + 1, combination + nextLetter)
         
-        backtrack('', 0)
-
+        backtrack(0, "")
         return ans
