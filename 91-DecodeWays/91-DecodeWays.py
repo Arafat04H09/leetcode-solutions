cdca9b9c143af741659fdc46d2c08bdead9fc5f8
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-29 18:56:23 -0500

    Update file 1/29/2025, 6:54:52 PM

--- 91-DecodeWays/91-DecodeWays.py
+++ 91-DecodeWays/91-DecodeWays.py
@@ -1,13 +1,19 @@
 class Solution(object):
     def numDecodings(self, s):
-        def decode_helper(index):
-            if index == len(s):
-                return 1  # Reached the end, one valid decoding
-            if s[index] == '0':
-                return 0  # If current digit is '0', it cannot be decoded alone
-            ways = decode_helper(index + 1)  # Single-digit decoding
-            if index + 1 < len(s) and int(s[index:index+2]) <= 26:
-                ways += decode_helper(index + 2)  # Two-digit decoding
-            return ways
-        return decode_helper(0)
-        
+        if not s or s[0] == '0':
+            return 0
+
+        n = len(s)
+        prev2, prev = 1, 1
+
+        for i in range(1, n + 1):
+            cur = 0
+            if s[i - 1] != '0':
+                cur += prev
+            
+            if i > 1 and 10 <= int(s[i - 2: i]) <= 26:
+                cur += prev2
+            
+            prev, prev2 = cur, prev
+            
+        return prev
