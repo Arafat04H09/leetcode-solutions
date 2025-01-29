1e1131c6315bb382349983c1bf22f21cddef4a6f
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-29 15:44:40 -0500

    Update file 1/29/2025, 3:43:08 PM

--- 56-MergeIntervals/56-MergeIntervals.py
+++ 56-MergeIntervals/56-MergeIntervals.py
@@ -4,23 +4,27 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        m = defaultdict(int)
+        if not intervals:
+            return []
 
-        for x,y in intervals:
-            m[x] += 1
-            m[y] -= 1
+        line = defaultdict(int)
+
+        for start, end in intervals:
+            line[start] += 1
+            line[end] -= 1
         
         cur = 0
-        intervalStart = -1
         ans = []
+        start = None
 
-        for num in sorted(m.keys()):
-            if cur == 0: 
-                intervalStart = num
+        for point in sorted(line.keys()):
+            if start is None and cur == 0:
+                start = point
+            
+            cur += line[point]
 
-            cur += m[num]
-
-            if cur == 0:  
-                ans.append([intervalStart, num])
+            if start is not None and cur == 0:
+                ans.append([start, point]) #end
+                start = None
         
         return ans
