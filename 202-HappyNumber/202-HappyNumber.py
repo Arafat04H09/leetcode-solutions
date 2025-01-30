f9f87d00723bedc73de5c88c6699a8645f2b9218
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-30 16:56:45 -0500

    Update file 1/30/2025, 4:55:12 PM

--- 202-HappyNumber/202-HappyNumber.py
+++ 202-HappyNumber/202-HappyNumber.py
@@ -8,19 +8,15 @@ class Solution(object):
 
         while n != 1:
             if n in memo:
-                return False  
+                return False
             
             memo.add(n)
             temp = 0
 
             while n > 0:
-                digit = n % 10
-                temp += digit ** 2
+                temp += (n % 10) ** 2
                 n //= 10
             
             n = temp
-
+        
         return True
-
-                      
-                 
