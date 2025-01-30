19c2bfd5100190b41f9959458ba144e48e93170d
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-30 14:16:51 -0500

    Update file 1/30/2025, 2:15:16 PM

--- 2068-CheckWhetherTwoStringsareAlmostEquivalent/2068-CheckWhetherTwoStringsareAlmostEquivalent.py
+++ 2068-CheckWhetherTwoStringsareAlmostEquivalent/2068-CheckWhetherTwoStringsareAlmostEquivalent.py
@@ -1,15 +1,3 @@
-class Solution(object):
-    def checkAlmostEquivalent(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: bool
-        """
-        c1 = Counter(word1)
-        c2 = Counter(word2)
-
-        for ch in "abcdefghijklmnopqrstuvwxyz":
-            if abs(c1[ch] - c2[ch]) > 3:
-                return False
-        
-        return True
+class Solution:
+    def checkAlmostEquivalent(self, w1: str, w2: str) -> bool:
+		return all(v < 4 for v in ((Counter(w1) - Counter(w2)) + (Counter(w2) - Counter(w1))).values())
