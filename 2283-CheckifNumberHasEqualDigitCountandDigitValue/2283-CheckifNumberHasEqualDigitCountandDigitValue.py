28be032a2df457642940fd74fff924328fd5e676
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-31 19:44:24 -0500

    Update file 1/31/2025, 7:42:52 PM

--- 2283-CheckifNumberHasEqualDigitCountandDigitValue/2283-CheckifNumberHasEqualDigitCountandDigitValue.py
+++ 2283-CheckifNumberHasEqualDigitCountandDigitValue/2283-CheckifNumberHasEqualDigitCountandDigitValue.py
@@ -1,7 +1,1 @@
-class Solution(object):
-    def digitCount(self, num):
-        """
-        :type num: str
-        :rtype: bool
-        """
-        return num in ["1210", "2020" , "21200", "3211000", "42101000", "521001000", "6210001000"]
+d.get(str(i), 0)
