e630fd7494ec4230b7a8508a946fe876ab8c9d10
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-02-01 10:48:41 -0500

    Update file 2/1/2025, 10:47:09 AM

--- 2415-ReverseOddLevelsofBinaryTree/2415-ReverseOddLevelsofBinaryTree.py
+++ 2415-ReverseOddLevelsofBinaryTree/2415-ReverseOddLevelsofBinaryTree.py
@@ -10,27 +10,15 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: Optional[TreeNode]
         """
-        queue = [root]
-        reverse = False
-
-        while queue:
-            nextQueue = []
-            curLevel = []
-
-            for node in queue:
-                curLevel.append(node.val)
-
-                if node.left:
-                    nextQueue.append(node.left)
-                if node.right:
-                    nextQueue.append(node.right)
+        def dfsHelper(left, right, reverse):
+            if not left or not right:
+                return
             
             if reverse:
-                l = len(curLevel) - 1
-                for node in queue:
-                    node.val = curLevel[l]
-                    l -= 1
-            queue = nextQueue
-            reverse = not reverse
+                left.val, right.val = right.val, left.val
+            
+            dfsHelper(left.left, right.right, not reverse)
+            dfsHelper(left.right, right.left, not reverse)
         
+        dfsHelper(root.left, root.right, True)
         return root
