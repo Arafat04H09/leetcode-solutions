8069023f3ef63f4f4e14d0e735e8feb5267a3ef2
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-02-01 14:38:07 -0500

    Update file 2/1/2025, 2:36:36 PM

--- 357-CountNumberswithUniqueDigits/357-CountNumberswithUniqueDigits.py
+++ 357-CountNumberswithUniqueDigits/357-CountNumberswithUniqueDigits.py
@@ -1,23 +1,19 @@
-class Solution(object):
-    def countNumbersWithUniqueDigits(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        if n == 0:
-            return 1
-
-        def makeUnique(numDigits):
-            if numDigits == 1:
-                return 10
-            ans = 9
-            digitsLeft = numDigits - 1
-            numsLeftToPick = 9
-            while digitsLeft > 0:
-                ans *= numsLeftToPick
-                numsLeftToPick -= 1
-                digitsLeft -= 1
-
-            return ans + makeUnique(numDigits - 1)
-        
-        return makeUnique(n)
+public int countNumbersWithUniqueDigits(int n) {
+    if (n==0){
+        return 1;
+    }
+    if (n>10){
+        return countNumbersWithUniqueDigits(10);
+    }
+    int sum = 1;
+    int k =0;
+    while(k<n){
+        if(k==0){
+            sum*=9;
+        }else{
+            sum*=(10-k);
+        }
+        k++;
+    }
+    return sum+countNumbersWithUniqueDigits(n-1);
+}
