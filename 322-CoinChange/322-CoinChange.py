1e357ce7a3faaa22a263518a8333066939e358d5
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-01-31 19:34:57 -0500

    Update file 1/31/2025, 7:33:26 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -5,13 +5,16 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        rs = [amount+1] * (amount+1)
-        rs[0] = 0
-        for i in xrange(1, amount+1):
-            for c in coins:
-                if i >= c:
-                    rs[i] = min(rs[i], rs[i-c] + 1)
 
-        if rs[amount] == amount+1:
-            return -1
-        return rs[amount]
+        if amount == 0:
+            return  0
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(amount + 1):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        
+        return dp[-1] if dp[-1] != float('inf') else -1
