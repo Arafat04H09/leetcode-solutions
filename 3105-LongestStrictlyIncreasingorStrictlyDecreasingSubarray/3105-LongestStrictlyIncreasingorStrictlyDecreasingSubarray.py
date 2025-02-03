b24461d2af1084ee938ea3c6ef51a1e72bfd6f7b
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-02-02 19:13:29 -0500

    Update file 2/2/2025, 7:11:59 PM

--- 3105-LongestStrictlyIncreasingorStrictlyDecreasingSubarray/3105-LongestStrictlyIncreasingorStrictlyDecreasingSubarray.py
+++ 3105-LongestStrictlyIncreasingorStrictlyDecreasingSubarray/3105-LongestStrictlyIncreasingorStrictlyDecreasingSubarray.py
@@ -1,29 +1,21 @@
-class Solution(object):
-    def longestMonotonicSubarray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        m = 0
-        c = 0
-        prev = float("inf")
-
-        for num in nums: #decreasing 
-            if num < prev:
-                c += 1
-                m = max(c, m)
-            else:
-                c = 1
-            prev = num
-        
-        c = 0
-        prev = -float("inf")
-        for num in nums:
-            if num > prev:
-                c += 1
-                m = max(c, m)
-            else:
-                c = 1
-            prev = num
-
-        return m
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int inc = 1, dec =  1;
+        int res = 1;
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i]>nums[i-1]){
+                inc+=1;
+                dec=1;
+            } else if(nums[i]<nums[i-1]){
+                dec+=1;
+                inc=1;
+            } else{
+                inc=1;
+                dec = 1;
+            }
+            
+            res = Math.max(res, Math.max(inc, dec));
+        }
+        return res;
+    }
+}
