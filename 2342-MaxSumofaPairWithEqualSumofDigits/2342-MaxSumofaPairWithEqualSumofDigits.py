5baac1a1b57506625f48aaa3bd719d3bb23bd9e5
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-02-11 19:42:06 -0500

    File updated at 2/11/2025, 7:40:33 PM

--- 2342-MaxSumofaPairWithEqualSumofDigits/2342-MaxSumofaPairWithEqualSumofDigits.py
+++ 2342-MaxSumofaPairWithEqualSumofDigits/2342-MaxSumofaPairWithEqualSumofDigits.py
@@ -7,7 +7,11 @@ class Solution(object):
         res = -1
         m = defaultdict(lambda: [0, 0])  
         for num in nums:
-            s = sum(int(digit) for digit in str(num))
+            s = 0
+            x = num
+            while x > 0:
+                s += x % 10
+                x //= 10
             
             if num > m[s][0]:
                 m[s][1] = m[s][0]
