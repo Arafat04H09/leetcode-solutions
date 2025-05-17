755255e8066cd30ccdf1a8be70a854a6e39eeb29
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-05-17 16:02:39 -0400

    File updated at 5/17/2025, 3:59:58 PM

--- 75-SortColors/75-SortColors.py
+++ 75-SortColors/75-SortColors.py
@@ -1,20 +1,21 @@
+# Last updated: 5/17/2025, 3:59:58 PM
 class Solution(object):
     def sortColors(self, nums):
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
-        l, r = 0, len(nums) - 1
-        i = 0
+        zeros, twos = 0, len(nums) - 1
+        cur = 0
 
-        while i <= r:
-            if nums[i] == 0:
-                nums[l], nums[i] = nums[i], nums[l]
-                l += 1        
-            elif nums[i] == 2:
-                nums[r], nums[i] = nums[i], nums[r]   
-                r -= 1
-                i -= 1
-            i += 1
+        while cur <= twos:
+            if nums[cur] == 0:
+                nums[zeros], nums[cur] = nums[cur], nums[zeros]
+                zeros += 1
+            elif nums[cur] == 2:
+                nums[twos], nums[cur] = nums[cur], nums[twos]
+                twos -= 1
+                cur -= 1
+            cur += 1
         
+        return nums
