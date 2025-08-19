762e03870fedb88c5b3f967ed381ba99e8932082
Arafat Hossain <91175479+Arafat04H09@users.noreply.github.com>
2025-08-19 00:56:29 -0400

    File updated at 8/19/2025, 12:57:23 AM

--- 71-SimplifyPath/71-SimplifyPath.py
+++ 71-SimplifyPath/71-SimplifyPath.py
@@ -1,10 +1,18 @@
-def simplifyPath(self, path):
-    stack = []
-    for token in path.split('/'):
-        if token in ('', '.'):
-            pass
-        elif token == '..':
-            if stack: stack.pop()
-        else:
-            stack.append(token)
-    return '/' + '/'.join(stack)
+# Last updated: 8/19/2025, 12:57:23 AM
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        stack = []
+
+        for token in path.split('/'):
+            if token in ['', '.']:
+                continue
+            elif token == '..':
+                if stack: stack.pop()
+            else:
+                stack.append(token)
+        
+        return '/' + '/'.join(stack)
